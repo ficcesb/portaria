55e976a538e20a717ac137362bdcd185cf1dcd9a
Nalisson Gomes <nalissongm.pro@gmail.com>
2024-04-05 10:39:52 -0300

    agora sim terminei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import Success from "@/components/ui/success";
+import { redirect } from 'next/navigation';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 type SubmitFormData = {
   cpf: string;
@@ -38,38 +41,7 @@ export default function Home() {
   })
 
   function onSubmit(e: SubmitFormData) {
-    setIsLoading(true);
-
-    const dataForm = new FormData();
-    const data = Object.values({...form.getValues()})
-
-    Object.keys(form.getValues()).forEach((key,i) => {
-      if(key !== "file"){
-        dataForm.append(key, data[i]);
-      }
-    })
-
-    const fetchAll = new Promise(async () => {
-      const result = await fetch("/api/alunos", {
-        method: "POST",
-        body: dataForm
-      })
-
-      if(result.status === 201) {
-        setState("CREATED");
-      }
-
-      if(result.status === 400) {
-        const txtError = await result.text()
-
-        setState(() => "ERROR");
-        form.setError("cpf", {type: "error", message: txtError});
-
-        setIsLoading(false);
-      }
-    })
-    console.log("SUBMIT CHAMADO")
-    Promise.resolve(fetchAll)
+    setState("PROGRESSING")
   }
 
   useEffect(() => {
@@ -78,8 +50,36 @@ export default function Home() {
 
   useEffect(() => {
     const fetchAll = async () => {
+      if(state === "PROGRESSING"){
+        const dataForm = new FormData();
+        const data = Object.values({...form.getValues()})
+        
+        Object.keys(form.getValues()).forEach((key,i) => {
+          if(key !== "file"){
+            dataForm.append(key, data[i]);
+          }
+        })
+
+        const result = await fetch("/api/alunos", {
+          method: "POST",
+          body: dataForm
+        })
+  
+        if(result.status === 201) {
+          setState("CREATED");
+        }
+  
+        if(result.status === 400) {
+          const txtError = await result.text()
+  
+          setState(() => "ERROR");
+          form.setError("cpf", {type: "error", message: txtError});
+  
+          setIsLoading(false);
+        }
+      }
+
       if(state === "CREATED"){ 
-        console.log("CREATED CHAMADO")
         const formData = new FormData();
         
         formData.append("file", file[0]);
@@ -103,8 +103,7 @@ export default function Home() {
         }
       }
       
-      if(state === "UPLOADED"){ 
-        console.log("CHAMADO CHAMADO")
+      if(state === "UPLOADED"){
         const formData = new FormData();
         const data = Object.values({...form.getValues()})
       
@@ -145,99 +144,104 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col items-center justify-between">
-       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full max-w-[350px] max-sm:px-4 pt-20 pb-20">
-          <FormField
-            control={form.control}
-            name="cpf"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>CPF</FormLabel>
-                <FormControl>
-                  <Input 
-                    placeholder="ex: 12345678900" {...field} 
-                    onChange={handleChange} 
-                    maxLength={14}
-                    type="text"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem >
-                <FormLabel>Nome Completo</FormLabel>
-                <FormControl>
-                  <Input placeholder="ex: 12345678900" {...field} type="text"/>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="matricula"
-            render={({ field }) => (
-              <FormItem >
-                <FormLabel>Matricula</FormLabel>
-                <FormControl>
-                  <Input placeholder="ex: 12345678 (Opcional)" {...field} type="text" required={false}/>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="file"
-            render={({ field }) => (
-              <FormItem 
-                className="flex w-full justify-between items-center gap-12 mb-12"
-              >
-                <FormLabel className="whitespace-nowrap mt-2">Enviar foto</FormLabel>
-                <FormControl>
-                  <Input {...field} className="w-full" type="file" accept="image/png,image/jpg,image/jpeg" onChange={(e) => {if(e.target.files) setFile(e.target.files)}} required/>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="pt-12">
-            <Button disabled={isLoading} type="submit" className="w-full py-4">
-              {!isLoading && "Realizar cadastro"}
-              <ThreeDots
-                visible={isLoading}
-                height="30"
-                width="30"
-                color="#fff"
-                radius="9"
-                ariaLabel="three-dots-loading"
-                wrapperStyle={{}}
-                wrapperClass=""
-              />
-            </Button>
-          </div>
-        </form>
-    </Form>
-      <Dialog open={dialogModal} onOpenChange={setDialogModal}>
-      <DialogContent className="max-sm:h-svh flex flex-col justify-between" >
-        <DialogHeader className="flex-1">
-          <DialogDescription className="flex flex-col items-center justify-center h-100%">
-            <Success className="w-[80%] h-auto"/>
-            <h1 className="font-bold text-2xl text-black">🎉 Parabéns! 🎉</h1>
-            <p className="text-center text-lg mt-4 mb-2 px-2">Se cadastro foi realizado com sucesso! Já estamos de olho e em breve já será possível ingressar via leitura facial.</p>
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter>
-          <Button type="button" className="w-full" onClick={() => setDialogModal(false)}>Voltar</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <div className='flex h-screen justify-between' style={{flexDirection: 'column'}}>
+      <main className="w-full flex flex-col items-center">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full max-w-[350px] max-sm:px-4 pt-20 pb-20">
+            <FormField
+              control={form.control}
+              name="cpf"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>CPF</FormLabel>
+                  <FormControl>
+                    <Input 
+                      placeholder="ex: 12345678900" {...field} 
+                      onChange={handleChange} 
+                      maxLength={14}
+                      type="text"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem >
+                  <FormLabel>Nome Completo</FormLabel>
+                  <FormControl>
+                    <Input placeholder="ex: 12345678900" {...field} type="text"/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="matricula"
+              render={({ field }) => (
+                <FormItem >
+                  <FormLabel>Matricula</FormLabel>
+                  <FormControl>
+                    <Input placeholder="ex: 12345678 (Opcional)" {...field} type="text" required={false}/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => (
+                <FormItem 
+                  className="flex w-full justify-between items-center gap-12 mb-12"
+                >
+                  <FormLabel className="whitespace-nowrap mt-2">Enviar foto</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="w-full" type="file" accept="image/png,image/jpg,image/jpeg" onChange={(e) => {if(e.target.files) setFile(e.target.files)}} required/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="pt-12">
+              <Button disabled={state=== "PROGRESSING" || state === "CREATED" || state === "UPLOADED"} type="submit" className="w-full py-4">
+                {!isLoading && "Realizar cadastro"}
+                <ThreeDots
+                  visible={isLoading}
+                  height="30"
+                  width="30"
+                  color="#fff"
+                  radius="9"
+                  ariaLabel="three-dots-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                />
+              </Button>
+            </div>
+          </form>
+      </Form>
+        <Dialog open={dialogModal} onOpenChange={setDialogModal}>
+        <DialogContent className="max-sm:h-svh flex flex-col justify-between" >
+          <DialogHeader className="max-sm:flex-1"> 
+            <DialogDescription className="flex max-sm:flex-1 flex-col items-center justify-center">
+              <Success className="w-[80%] h-auto"/>
+              <h1 className="font-bold text-2xl text-black">🎉 Parabéns! 🎉</h1>
+              <p className="text-center text-lg mt-4 mb-2 px-2">Se cadastro foi realizado com sucesso! Já estamos de olho e em breve já será possível ingressar via leitura facial.</p>
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button type="button" className="w-full max-sm:py-5 max-sm:text-lg mb-4" onClick={() => setDialogModal(false)}>Voltar</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </main>
+    <footer className='flex w-full justify-center py-8 border-t-[1px]'>
+      <Link href="http://links.nalissongomes.me" target="_blank" className='cursor-pointer text-stone-400 text-sm font-light whitespace-pre'>Developed by <span className='text-slate-900 text-sm font-normal'>Nalisson Gomes</span></Link>
+    </footer>
+    </div>
   );
 }
